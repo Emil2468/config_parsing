3e6c63ec3eecdb31976da3e9c9a63d3ddc98716c
Emil Møller Hansen <emh@calaha.dk>
2023-07-31 14:22:51 +0200

    initial commit

---
+++ config_parsing/__init__.py
@@ -0,0 +1,3 @@
+from .arg_helpers import split_args, update_conf_with_parsed_args
+from . import config_parser
+from .base_config import BaseConfig
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='config_parsing',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Library for parsing json files to config classes',
+    author='Mig og min ven Simon',
+    license='MIT',
+)
